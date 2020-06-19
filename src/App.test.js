df1f4f19c67cab34cd7dde47c1d0d6bc0621c0ac
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2020-06-19 12:33:48 +0300

    Сохранено 19.06.2020 года

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App'
+import Character from './Character'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({
+  adapter: new Adapter()
+})
+
+describe('<App />', () => {
+
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+
+  it('Компонент App должен отрендерить 2 персонажа на светлой стороне', () => {
+    expect(wrapper.find(Character)).toHaveLength(2)
+  })
+
+  it('Компонент App должен отрендерить 3 персонажа на темной стороне', () => {
+    wrapper.setProps({
+      side: 'dark'
+    })
+    expect(wrapper.find(Character)).toHaveLength(3)
+  })
+
+})
+
+
+// describe('<App />', () => {
+
+//   it('Компонент App должен отрендерить 2 персонажа на светлой стороне', () => {
+
+//     const wrapper = shallow(<App />)
+
+//     expect(wrapper.find(Character)).toHaveLength(2)
+//   })
+
+//   it('Компонент App должен отрендерить 3 персонажа на темной стороне', () => {
+
+//     const wrapper = shallow(<App side={'dark'} />)
+
+//     expect(wrapper.find(Character)).toHaveLength(3)
+//   })
+
+// })
